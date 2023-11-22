71464705131aed78f4041c15fbc4c5d4a4ebbf2e
21020303 <21020303@vnu.edu.vn>
2023-11-22 16:22:16 +0700

    Header and Sidebar for Warehouse

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from './distribution_center/pages/Dashboard';
 import CreateDelivery from './distribution_center/pages/CreateDelivery';
 import DeliveryManagement from './distribution_center/pages/DeliveryManagement';
+import Warehouse from './warehouse/pages/Dashboard';
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                     <Route path="/distribution/dashboard" element={<Dashboard />} />
                     <Route path="/distribution/createdelivery" element={<CreateDelivery />} />
                     <Route path="/distribution/deliverymanagement" element={<DeliveryManagement />} />
+                    <Route path="/warehouse/dashboard" element={<Warehouse />} />
                 </Routes>
             </BrowserRouter>
         </div>
---
+++ frontend/src/warehouse/pages/Dashboard/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '~/warehouse/layout';
+
+const Warehouse = () => {
+    return (
+        <Layout>
+            <div>Home</div>
+        </Layout>
+    );
+};
+
+export default Warehouse;
