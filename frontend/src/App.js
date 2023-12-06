ce522c36fea3a155000ad968c7a370c008a4d1c9
Tran Duc Khai / チャン.ドゥック.カイ <89479879+duckhai2323@users.noreply.github.com>
2023-12-07 01:14:39 +0700

    Create Route distribution-admin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,34 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Dashboard from './distribution_center/pages/Dashboard';
+import DashboardDDD from './distribution_center/pages/DashboardDDD';
 import CreateDelivery from './distribution_center/pages/CreateDelivery';
 import DeliveryManagement from './distribution_center/pages/DeliveryManagement';
-import Warehouse from './warehouse/pages/Dashboard';
+import ReceivedGoods from './distribution_center/pages/ReceivedGoods';
 
 import Add from './warehouse/pages/ManageStaff/Add/Add';
 import ManageStaff from './warehouse/pages/ManageStaff/ManageStaff';
 import ChangeInfor from './warehouse/pages/ManageStaff/ChangeInfor/ChangeInfor';
+import Warehouse from './warehouse/pages/Dashboard';
+
+import Dashboard from './distribution_admin/pages/Dashboard';
+import Employees from './distribution_admin/pages/Employees';
 
 function App() {
     return (
         <div>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/distribution/dashboard" element={<Dashboard />} />
+                    <Route path="/distribution/dashboard" element={<DashboardDDD />} />
                     <Route path="/distribution/createdelivery" element={<CreateDelivery />} />
                     <Route path="/distribution/deliverymanagement" element={<DeliveryManagement />} />
-                    <Route path="/warehouse/dashboard" element={<Warehouse/>} />
-                    <Route path="/warehouse/managestaff" element={<ManageStaff/>} />
-                    <Route path="/warehouse/managestaff/add" element={<Add/>} />
-                    <Route path="/warehouse/managestaff/changeInfor" element={<ChangeInfor/>} />
+                    <Route path="/distribution/receivedgoods" element={<ReceivedGoods />} />
+
+                    <Route path="/admin/distribution/dashboard" element={<Dashboard />} />
+                    <Route path="/admin/distribution/employees" element={<Employees />} />
+
+                    <Route path="/warehouse/dashboard" element={<Warehouse />} />
+                    <Route path="/warehouse/managestaff" element={<ManageStaff />} />
+                    <Route path="/warehouse/managestaff/add" element={<Add />} />
+                    <Route path="/warehouse/managestaff/changeInfor" element={<ChangeInfor />} />
                 </Routes>
             </BrowserRouter>
         </div>
---
+++ frontend/src/distribution_admin/pages/Dashboard/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Layout from '~/distribution_admin/layout';
+
+const Dashboard = () => {
+    return <Layout>Hello Amin</Layout>;
+};
+
+export default Dashboard;
