e561cece234e2fcc122e041594ad85307118410e
CaoDuc11 <100215547+CaoDuc11@users.noreply.github.com>
2023-12-28 23:03:11 +0700

    UI

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -7,7 +7,8 @@ const images = {
     tinh_thanh: require('~/assets/63tinh-thanh.png'),
     xe: require('~/assets/1000xe.png'),
     buu_cuc: require('~/assets/1900bu-cuc.png'),
-    nhan_vien: require('~/assets/25000nhan-vien.png')
+    nhan_vien: require('~/assets/25000nhan-vien.png'),
+    map: require('~/assets/map.jpg')
 };
 
 export default images;
