d90207c3966ca01643202c4599096b5f11363e1b
Tran Duc Khai / チャン.ドゥック.カイ <89479879+duckhai2323@users.noreply.github.com>
2023-12-29 02:59:29 +0700

    Update DeliveriesShip - Router EmployeeDistribution

--- frontend/src/distribution_center/pages/ReceivedGoods/ReceivedTable/ReceivedTable.js
+++ frontend/src/distribution_center/pages/ReceivedGoods/ReceivedTable/ReceivedTable.js
@@ -1,73 +1,196 @@
 import React from 'react';
 import styles from './ReceivedTable.module.scss';
 import classNames from 'classnames/bind';
+import { RiDeleteBin6Line } from 'react-icons/ri';
+import { IoPaperPlane } from 'react-icons/io5';
 
 const cx = classNames.bind(styles);
+const Button = ({ item, onClick }) => {
+    switch (item.journeyStatus) {
+        case '6':
+            return (
+                <button
+                    style={{
+                        background: 'rgb(255, 95, 31)',
+                        color: 'white',
+                        fontSize: '13px',
+                        fontWeight: '500',
+                        padding: '0.4rem 0.5rem',
+                        border: 'none',
+                        borderRadius: '2px',
+                        cursor: 'pointer',
+                    }}
+                    onClick={() => onClick(item)}
+                >
+                    Xác nhận
+                </button>
+            );
+        case '7':
+            return (
+                <button
+                    style={{
+                        background: 'white',
+                        color: 'rgb(51,255,51)',
+                        fontSize: '14px',
+                        fontWeight: '500',
+                        padding: '0.4rem 0.5rem',
+                        border: 'none',
+                        borderRadius: '2px',
+                        cursor: 'pointer',
+                    }}
+                >
+                    Đã tới điểm giao dịch!
+                </button>
+            );
+        case '8':
+            return (
+                <button
+                    style={{
+                        background: 'white',
+                        color: 'hsl(187, 85%, 43%)',
+                        fontSize: '14px',
+                        fontWeight: '500',
+                        padding: '0.4rem 0.5rem',
+                        border: 'none',
+                        borderRadius: '2px',
+                        cursor: 'pointer',
+                    }}
+                >
+                    Đã chuyển hàng tới Shipper!
+                </button>
+            );
+        case '9':
+            return (
+                <button
+                    style={{
+                        background: 'white',
+                        color: 'hsl(260, 50%, 60%)',
+                        fontSize: '14px',
+                        fontWeight: '500',
+                        padding: '0.4rem 0.5rem',
+                        border: 'none',
+                        borderRadius: '2px',
+                        cursor: 'pointer',
+                    }}
+                >
+                    Giao dịch thành công!
+                </button>
+            );
+        case '0':
+            return (
+                <button
+                    style={{
+                        background: 'white',
+                        color: 'rgb(222, 46, 46)',
+                        fontSize: '14px',
+                        fontWeight: '500',
+                        padding: '0.4rem 0.5rem',
+                        border: 'none',
+                        borderRadius: '2px',
+                        cursor: 'pointer',
+                    }}
+                >
+                    Giao dịch không thành công!
+                </button>
+            );
+        default:
+            return null;
+    }
+};
 
-const ReceivedTable = () => {
+const Content = ({ item, onClick, selectConfirm }) => {
+    switch (item.journeyStatus) {
+        case '6':
+            return (
+                <select disabled className={cx('select-filter')}>
+                    <option value="Mã đơn hàng">Chờ xác nhận</option>
+                </select>
+            );
+        case '7':
+            return (
+                <button
+                    style={{
+                        background: 'rgb(51,255,51)',
+                        color: 'white',
+                        fontSize: '14px',
+                        fontWeight: '500',
+                        padding: '0.4rem 0.5rem',
+                        border: 'none',
+                        borderRadius: '2px',
+                        cursor: 'pointer',
+                    }}
+                    onClick={() => onClick(item)}
+                >
+                    Gửi tới khách hàng
+                </button>
+            );
+        case '8':
+            return (
+                <select className={cx('select-filter')} onChange={(e) => selectConfirm(e, item)}>
+                    <option value="">---XÁC NHẬN GIAO DỊCH---</option>
+                    <option value="success">GIAO DỊCH THÀNH CÔNG</option>
+                    <option value="error">GIAO DỊCH THẤT BẠI</option>
+                </select>
+            );
+        case '9':
+            return (
+                <div className={cx('button-XL')} style={{ background: 'rgb(222, 46, 46)' }}>
+                    <RiDeleteBin6Line className={cx('icon')} />
+                    <span>XÓA</span>
+                </div>
+            );
+        case '0':
+            return (
+                <div className={cx('button-XL')} style={{ background: 'rgb(10, 130, 60)' }}>
+                    <IoPaperPlane className={cx('icon')} />
+                    <span>HOÀN ĐƠN</span>
+                </div>
+            );
+        default:
+            return null;
+    }
+};
+
+const ReceivedTable = (props) => {
+    const { deliveriesShip, handleOpen, onClick, selectConfirm } = props;
     return (
         <div className={cx('received-table')}>
             <table className={cx('table-list')}>
                 <thead>
                     <tr className={cx('header-table')}>
-                        <th style={{ textAlign: 'center' }}>STT</th>
                         <th>MÃ ĐƠN HÀNG</th>
                         <th>KHÁCH HÀNG</th>
                         <th>NGƯỜI NHẬN</th>
-                        <th style={{ width: '25rem', textAlign: 'start' }}>ĐỊA CHỈ GIAO HÀNG</th>
+                        <th style={{ width: '20rem', textAlign: 'start' }}>ĐỊA CHỈ GIAO HÀNG</th>
                         <th>SỐ ĐIỆN THOẠI</th>
-                        <th style={{ textAlign: 'center' }}>TÌNH TRẠNG</th>
+                        <th>TÌNH TRẠNG</th>
+                        <th>XỬ LÝ</th>
                     </tr>
                 </thead>
 
                 <tbody>
-                    <tr className={cx('body-table')}>
-                        <th style={{ textAlign: 'center' }}>1</th>
-                        <th style={{ color: 'rgb(51,255,51)', cursor: 'pointer' }}>A000452</th>
-                        <th>Trần Đức Khải</th>
-                        <th>Nguyễn Phú Trọng</th>
-                        <th>Số nhà 23 Đường Hai Bà Trưng, Quận Hai Bà Trưng, Thủ Đức Thành phố nhà bè</th>
-                        <th>0989789789</th>
-                        <th style={{ textAlign: 'center' }}>
-                            <button
-                                style={{
-                                    background: 'rgb(51,255,51)',
-                                    color: 'white',
-                                    fontSize: '13px',
-                                    fontWeight: '500',
-                                    padding: '0.5rem 0.5rem',
-                                    border: 'none',
-                                    borderRadius: '2px',
-                                }}
+                    {deliveriesShip.map((item, index) => (
+                        <tr className={cx('body-table')}>
+                            <th
+                                style={{ color: 'rgb(51,255,51)', cursor: 'pointer' }}
+                                onClick={() => handleOpen(index)}
                             >
-                                Đã giao hàng
-                            </button>
-                        </th>
-                    </tr>
-
-                    <tr className={cx('body-table')}>
-                        <th style={{ textAlign: 'center' }}>1</th>
-                        <th style={{ color: 'rgb(51,255,51)', cursor: 'pointer' }}>A000452</th>
-                        <th>Trần Đức Khải</th>
-                        <th>Nguyễn Phú Trọng</th>
-                        <th>Số nhà 23 Đường Hai Bà Trưng, Quận Hai Bà Trưng, Thủ Đức Thành phố nhà bè</th>
-                        <th>0989789789</th>
-                        <th style={{ textAlign: 'center' }}>
-                            <button
-                                style={{
-                                    background: 'rgb(51,255,51)',
-                                    color: 'white',
-                                    fontSize: '13px',
-                                    fontWeight: '500',
-                                    padding: '0.5rem 0.5rem',
-                                    border: 'none',
-                                    borderRadius: '2px',
-                                }}
-                            >
-                                Đã giao hàng
-                            </button>
-                        </th>
-                    </tr>
+                                A000452
+                            </th>
+                            <th>{item.senderName}</th>
+                            <th>{item.receiverName}</th>
+                            <th style={{ textAlign: 'start', width: '20rem' }}>{item.receiverAddress}</th>
+                            <th>{item.receiverPhone}</th>
+                            <th style={{ textAlign: 'center' }}>
+                                <Button item={item} onClick={onClick} />
+                            </th>
+                            <th>
+                                <div className={cx('select-container')}>
+                                    <Content item={item} onClick={onClick} selectConfirm={selectConfirm} />
+                                </div>
+                            </th>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
--- frontend/src/distribution_center/pages/ReceivedGoods/index.js
+++ frontend/src/distribution_center/pages/ReceivedGoods/index.js
@@ -1,24 +1,93 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Layout from '~/distribution_center/layout';
 import styles from './ReceivedGoods.module.scss';
 import classNames from 'classnames/bind';
 import ReceivedHeader from './ReceivedHeader/ReceivedHeader';
 import ReceivedTable from './ReceivedTable/ReceivedTable';
 import ModalDetails from './ModalDetails/ModalDetails';
+import FadeLoader from 'react-spinners/FadeLoader';
+import { useDispatch, useSelector } from 'react-redux';
+import { GetDeliveryShip, resetCreateStatus, UpdateJourneyShip } from '~/all/features/EmployeeDistributionSlice';
 const cx = classNames.bind(styles);
 
 const ReceivedGoods = () => {
-    return (
-        <div className={cx('receved-goods-container')}>
-            <Layout>
-                <div className={cx('received-goods-page')}>
-                    <ReceivedHeader />
-                    <ReceivedTable />
+    const { deliveriesShip, isLoading2, isSuccess2, createStatus } = useSelector((state) => state.employeeDistribution);
+    const dispatch = useDispatch();
+    const [position, setPosition] = useState(0);
+    const [displayModal, setDisplayModal] = useState('none');
+    useEffect(() => {
+        if (!isSuccess2 || createStatus === 'flex') {
+            const delay = setTimeout(() => {
+                dispatch(GetDeliveryShip());
+                setPosition(0);
+                dispatch(resetCreateStatus());
+            }, 1000);
+            return () => {
+                clearTimeout(delay);
+            };
+        }
+    }, [dispatch, isSuccess2, createStatus]);
+    const onClickSubmit = (item) => {
+        dispatch(UpdateJourneyShip(item));
+    };
+    const handleClickOpen = (index) => {
+        setPosition(index);
+        setDisplayModal('flex');
+    };
+    const CloseModal = () => {
+        setDisplayModal('none');
+    };
+    const selectConfirm = (e, item) => {
+        dispatch(UpdateJourneyShip({ ...item, confirm: e.target.value }));
+    };
+    let content;
+    if (isLoading2) {
+        content = (
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: '100vh',
+                    width: '100vw',
+                    background: 'hsl(200, 80%, 90%)',
+                }}
+            >
+                <FadeLoader color="rgba(255, 255, 255, 1)" height="10" width="6" />
+                <span style={{ fontWeight: '500', color: 'white', fontSize: '18px' }}>Loading...</span>
+            </div>
+        );
+    } else if (isSuccess2) {
+        content = (
+            <div className={cx('receved-goods-container')}>
+                <Layout>
+                    <div className={cx('received-goods-page')}>
+                        <ReceivedHeader />
+                        <ReceivedTable
+                            deliveriesShip={deliveriesShip}
+                            handleOpen={handleClickOpen}
+                            onClick={onClickSubmit}
+                            selectConfirm={selectConfirm}
+                        />
+                    </div>
+                </Layout>
+                <div style={{ display: createStatus }} className={cx('loading')}>
+                    <div>
+                        <FadeLoader color="rgba(255, 255, 255, 1)" height="10" width="6" />
+                        <span style={{ fontWeight: '500', color: 'white', fontSize: '18px' }}>Loading...</span>
+                    </div>
                 </div>
-            </Layout>
-            <ModalDetails className={cx('modal-details')} />
-        </div>
-    );
+                <ModalDetails
+                    className={cx('modal-details')}
+                    display={displayModal}
+                    deliveries={deliveriesShip}
+                    index={position}
+                    handleClose={CloseModal}
+                />
+            </div>
+        );
+    }
+    return <div>{content}</div>;
 };
 
 export default ReceivedGoods;
